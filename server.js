7b6ba537749e97906d32e9bed246b7796832a72f
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-11-14 09:57:56 -0500

    init commit

---
+++ server.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+const morgan = require("morgan");
+
+// const authRoutes = require('./routes/authRoutes');
+// const gameRoutes = require('./routes/gameRoutes');
+
+const session = require("express-session");
+app.use(session({ secret: "badabing", cookie: { maxAge: 3600000 } }));
+
+const cors = require("cors");
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+// app.use(authRoutes);
+// app.use(gameRoutes);
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+const PORT = process.env.PORT || 3600;
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
