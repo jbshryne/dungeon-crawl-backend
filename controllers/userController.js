00fff5a62df63a1d0767cf5d9bb4317e4ade4827
Jon Brennan <jonbrennan@Jons-MacBook-Pro.local>
2023-11-14 19:45:28 -0500

    signup & login are functional

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,37 +2,52 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+// const session = require("express-session")
+
+router.get("/login", (req, res) => {
+  res.json({ message: "login" });
+});
 
 router.post("/login", async (req, res) => {
+  console.log(req.body);
+
   let userToLogin = await User.findOne({ username: req.body.username });
 
   if (userToLogin) {
+    console.log(userToLogin);
     bcrypt.compare(req.body.password, userToLogin.password, (err, result) => {
       if (result) {
         req.session.userId = userToLogin._id;
         req.session.username = userToLogin.username;
-        res.redirect("/game");
+
+        res.json(userToLogin);
       } else {
-        res.send("wrong password");
+        res.status(401).json({ message: "Incorrect Password" });
       }
     });
   }
 });
 
-router.post("/signup", async (req, res) => {
-  if (req.body.username && req.body.password) {
-    bcrypt.hash(req.body.password, 10, async (err, hashed) => {
-      req.body.password = hashed;
+router.get("/signup", (req, res) => {
+  res.json({ message: "signup" });
+});
 
+router.post("/signup", async (req, res) => {
+  console.log(req.body);
+  if (req.body.username && req.body.password) {
+    let plainTextPassword = req.body.password;
+    bcrypt.hash(plainTextPassword, 10, async (err, hashedPassword) => {
+      req.body.password = hashedPassword;
       await User.create(req.body);
-      res.redirect("/auth");
+
+      res.json({ message: "User created successfully" });
     });
   }
 });
 
 router.get("/logout", (req, res) => {
   req.session.destroy();
-  res.redirect("/login");
+  res.json({ message: "Logout successful" });
 });
 
 module.exports = router;
